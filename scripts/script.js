c864a5960948e3605de587944d5a8a1fbaa696ad
Zaya-R2005 <zaijevrodion0413@gmail.com>
2025-04-03 17:25:50 +0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -2,4 +2,17 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log('Спасибо за ваше сообщение!')
 });
+// Получаем элемент навигационного меню
+const nav = document.querySelector('nav');
 
+// Определяем начальную позицию
+const sticky = nav.offsetTop;
+
+// Добавляем слушатель события прокрутки
+window.addEventListener('scroll', () => {
+    if (window.pageYOffset > sticky) {
+        nav.classList.add('fixed');
+    } else {
+        nav.classList.remove('fixed');
+    }
+});
